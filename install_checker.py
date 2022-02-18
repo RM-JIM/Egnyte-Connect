5daa5587b532d08e755c4052d9197cf2d2f6d2c3
RM-JIM <99673653+RM-JIM@users.noreply.github.com>
2022-02-18 15:25:15 +0000

    Update install_checker.py

--- install_checker.py
+++ install_checker.py
@@ -9,7 +9,6 @@ if current_os >= min_os:
     exit(0)
 elif current_os < min_os:
     print("Current installed OS is older than Catalina, so is not compatible for Egnyte install")
-    os.system(""" osascript -e 'display notification "Unable to install Egnyte due to incompatable OS version" with title "Unable To Install Egnyte"'""")
     exit(1)
 else:
     print("Error has occured, exiting script and install")
