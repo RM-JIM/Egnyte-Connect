23b1bbe175f1cfc14a1e0846717356923d3243f7
RM-JIM <99673653+RM-JIM@users.noreply.github.com>
2022-02-18 17:05:08 +0000

    Update install_checker.py

--- install_checker.py
+++ install_checker.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 import os
+import platform
 
 min_os = 10.15
-current_os = os.system("sw_vers -productVersion")
+installed_os = str(os.system("sw_vers -productVersion"))
+running_ver = platform.mac_ver()[0]
 
-if current_os >= min_os:
+if running_ver >= min_os:
     print("Current installed OS is newer than Mojave, so is compatible for install")
     exit(0)
-elif current_os < min_os:
+elif running_ver < min_os:
     print("Current installed OS is older than Catalina, so is not compatible for Egnyte install")
+    #os.system(""" osascript -e 'display notification "Unable to install Egnyte due to incompatable OS version" with title "Unable To Install Egnyte"'""")
     exit(1)
 else:
     print("Error has occured, exiting script and install")
